8f7706746c84031f4ebde342f87866bfe66c3a06
klekkwedge <antonov.andrey.10101@gmail.com>
2022-09-07 11:04:42 +0300

    feat: 1 lesson

---
+++ 01 Introduction/src/Family.java
@@ -0,0 +1,9 @@
+public class Family {
+    public static void main(String[] args) {
+        System.out.println("Alex");
+        System.out.println("John");
+        System.out.println("Olivia");
+        System.out.println("Emma");
+        System.out.println("Ben");
+    }
+}
